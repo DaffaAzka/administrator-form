d5a902cd8792e26fd8028ca6ba4980c9d7cff21b
Daffa Islami Azka <azkadaiki26@gmail.com>
2024-02-19 05:06:23 +0700

    0.1
    Create config databases and user model

---
+++ Administrator/Data/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Administrator.Data
+{
+    public class User
+    {
+        int ID {  get; set; }
+        string EmployeeID {  get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        string Fullname { get; set; }
+        string Status { get; set; }
+        string Role { get; set; }
+        string DateEntry { get; set; }
+        string DateOut { get; set; }
+
+    }
+}
---
+++ Administrator/Databases/UserDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administrator.Databases
+{
+    public class UserDatabase
+    {
+        public static string myConnection = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString;
+
+    }
+}
